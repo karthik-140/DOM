2060f6fec3c47c1ab9d9a68565333f7be830d455
karthik-140 <shanigaramkarthik@gmail.com>
2022-12-27 15:26:05 +0530

    task6

---
+++ queryselector.js
@@ -0,0 +1,7 @@
+var ans=document.querySelector('.list-group-item:nth-child(2)');
+ans.style.background='green';
+
+var del=document.querySelector('.list-group-item:nth-child(3)');
+del.remove();
+
+
