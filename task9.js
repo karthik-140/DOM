1cde9f0a9cdd03d1655f184ebda2595ed61e2df0
karthik-140 <shanigaramkarthik@gmail.com>
2022-12-28 16:42:53 +0530

    question2

--- task9.js
+++ task9.js
@@ -1,24 +1,37 @@
-var filter=document.getElementById('filter');
-filter.addEventListener('keyup', addfilter);
-function addfilter(e){
-    e.preventDefault();
-    var text=e.target.value.toLowerCase();
+// var filter=document.getElementById('filter');
+// filter.addEventListener('keyup', addfilter);
+// function addfilter(e){
+//     e.preventDefault();
+//     var text=e.target.value.toLowerCase();
     
-    var itemList=document.getElementById('items');
-    var items=(itemList.getElementsByTagName('li'));
+//     var itemList=document.getElementById('items');
+//     var items=(itemList.getElementsByTagName('li'));
    
-    Array.from(items).forEach(function(item){
-        var itemName=item.firstChild.textContent;
+//     Array.from(items).forEach(function(item){
+//         var itemName=item.firstChild.textContent;
        
-        if(itemName.toLowerCase().indexOf(text)!=-1)
-        {
-            item.style.display='block';
-        }
-        else{
-            item.style.display='none';
-        }
-    })
+//         if(itemName.toLowerCase().indexOf(text)!=-1)
+//         {
+//             item.style.display='block';
+//         }
+//         else{
+//             item.style.display='none';
+//         }
+//     })
+// }
+
+var form=document.getElementById('addForm');
+var itemlist=document.getElementById('items');
+
+form.addEventListener('submit', addtext)
+function addtext(e){
+    e.preventDefault();
+     
+    var item=document.getElementById('item').value;
+    var disc=document.getElementById('description').value;
+
+   var addli=document.createElement('li');
+   addli.className='list-group-item';
+   addli.appendChild(document.createTextNode(item+" "+ disc));
+    itemlist.appendChild(addli);
 }
-
-
-
